593e0d82ecd276d00ffea7e0ddc8cc41635a984a
Vineet <singhvineet.y17@gmail.com>
2024-08-28 01:21:23 -0400

    login page css fixed

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -17,6 +17,15 @@ const homepage=()=>{
                 Sign-up
             </button>
             <img src={art} className="homeart"/>
+            <div className="align"></div>
+            <footer class="footer">
+                <div class="waves">
+                <div class="wave" id="wave1"></div>
+                <div class="wave" id="wave2"></div>
+                <div class="wave" id="wave3"></div>
+                <div class="wave" id="wave4"></div>
+                </div>
+            </footer>
         </div>
         </>
     );
