815535f9873ba4d0d3964eab1056f5b04eae3cd6
shaikhjunaidx <shaikhjunaidx@gmail.com>
2024-08-29 00:01:06 -0400

    Set up routing and served the application in main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/shaikhjunaidx/pennywise-backend/db"
+	"github.com/shaikhjunaidx/pennywise-backend/internal/routes"
 )
 
 func main() {
 	database := db.InitDB()
-	fmt.Println(database.Name())
+	fmt.Println("Connected to the database:", database.Name())
+
+	router := mux.NewRouter()
+
+	routes.SetupUserRoutes(router, database)
+
+	log.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
